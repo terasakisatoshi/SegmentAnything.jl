86265191c19064fc57a519ec03e22f0956c26727
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2024-03-30 15:43:48 +0900

    Add check before downloading model

--- deps/build.jl
+++ deps/build.jl
@@ -4,5 +4,7 @@ filename = "sam_vit_h_4b8939.pth"
 url = "https://dl.fbaipublicfiles.com/segment_anything/$filename"
 path = joinpath(@__DIR__, filename)
 
-println("Downloading segment-anything dataset to $path")
-Downloads.download(url, path)
+if !isfile(path)
+	println("Downloading segment-anything dataset to $path")
+	Downloads.download(url, path)
+end
